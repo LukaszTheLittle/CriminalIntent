116e9ed56aaf5cb1512f185fa00844794b232e1c
LukaszTheLittle <lukis.maly@gmail.com>
2022-03-26 16:39:48 +0100

    Optimized imports (deleted wildcard imports)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -4,8 +4,7 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.Room
 import database.CrimeDatabase
-import java.lang.IllegalStateException
-import java.util.*
+import java.util.UUID
 
 private const val DATABASE_NAME = "crime-database"
 
